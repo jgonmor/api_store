365dcc327bf758232c5e97661eb0797634cb0454
jgonmor <jgonmor366@iessaladillo.es>
2024-12-13 19:44:35 +0100

    Unused import removed

--- src/main/java/com/jgonmor/store/service/client/ClientService.java
+++ src/main/java/com/jgonmor/store/service/client/ClientService.java
@@ -8,9 +8,7 @@ import com.jgonmor.store.repository.IClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ClientService implements IClientService{
